c99ad32482a67d7e422a70e75a2305f91cfb9b8d
Azzeddine <azzou.bob42@gmail.com>
2026-01-25 22:37:38 +0100

    Creating the trustedBy component and styling it

--- frontend/src/pages/HomePage/TrustedBy.jsx
+++ frontend/src/pages/HomePage/TrustedBy.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { getTrustedBy } from "../../api/get-trusted-by";
+import google from "../../assets/google.png";
+import amazon from "../../assets/amazon.png";
+import facebook from "../../assets/facebook.png";
+import twitter from "../../assets/twitter.png";
+import instagram from "../../assets/instagram.png";
 import './TrustedBy.css';
 
 function TrustedBy() {
@@ -16,11 +21,24 @@ function TrustedBy() {
     }
     fetchTrustedBy();
   }, []);
-  return (
-    <>
 
-    </>
-  );
+  return (
+    <section className="trusted-by">
+      <div className="trusted-by-container">
+        <h2>Trusted By</h2>
+        <div className="trusted-by-logos">
+          {/* {trustedBy.map((logo) => (
+            <img key={logo.id} src={logo.image} alt={logo.name} />
+          ))} */}
+          <img src={google} alt="google" />
+          <img src={amazon} alt="amazon" />
+          <img src={facebook} alt="facebook" />
+          <img src={twitter} alt="twitter" />
+          <img src={instagram} alt="instagram" />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default TrustedBy;
